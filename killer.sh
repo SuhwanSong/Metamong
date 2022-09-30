ec4b6bcb2a743e5391c89de054dbda3b2041ed60
Suhwan <prada960808@gmail.com>
2022-09-30 10:28:35 +0900

    killer

---
+++ killer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will kill process which running more than X hours
+# egrep: the selected process; grep: hours
+while true
+do
+  PIDS="`ps eaxo etimes,pid,comm | egrep "firefox|chrome" | awk ' { if ($1 > 7200) print $2 }'`"
+
+# Kill the process
+  for i in ${PIDS}; do { echo "Killing $i"; kill -15 $i; }; done;
+  sleep 60;
+done
